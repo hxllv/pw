3a0d8ec62a80864113313ec5c3e6abef8ea6cd30
hxllv <nace.tavcer20@gmail.com>
2021-10-19 23:23:46 +0200

    checkout backend complete

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Item;
 use Exception;
+use App\Mail\OrderMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\QueryException;
@@ -20,11 +22,30 @@ class CheckoutController extends Controller
 
     public function captcha()
     {
-        return Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
+        $captchaResponse = Http::asForm()->post('https://www.google.com/recaptcha/api/siteverify', [
             'secret' => "6LfYy7IcAAAAAODUQ8HhbRVp0m-Ss1WdGIGqGMUW",
             'response' => request('token'),
             'remoteip' => request()->ip()
         ])->json();
+
+        if ($captchaResponse['success']) {
+            Mail::to("nace.tavcer20@gmail.com")->send(new OrderMail(
+                [
+                    'userData' => session('userData'), 
+                    'items' => session('items'),
+                    'price' => session('price')
+                ]
+            ));
+
+
+            if(count(Mail::failures()) > 0){
+                return response()->json(['success' => false]);
+            }
+
+            return response()->json(['success' => true]);
+        }
+
+        return response()->json(['success' => false]);
     }
 
     public function checkItems()
@@ -34,7 +55,9 @@ class CheckoutController extends Controller
         ]);
 
         if ($validator->fails())
-            return response()->json(['error' => ['items' => 'Poslani podatki niso v obliki JSON, obrnite se na skrbnika spletne strani.']]);
+            return response()->json(['error' => [
+                'items' => 'Poslani podatki niso v obliki JSON, obrnite se na skrbnika spletne strani.'
+            ]]);
 
         $data = json_decode(request()->all()['items'], true);
 
@@ -42,18 +65,22 @@ class CheckoutController extends Controller
             return response()->json(['error' => ['items' => 'V košarici ni izdelkov.']]);
 
         $items = [];
+        $price = 0;
         foreach ($data as $key => $itemNotUsed) {
             $item = Item::find($key);
 
             if ($item === null)
-                return response()->json(['error' => ['items' => 'Izdelek v košarici ima neveljaven ID, obrnite se na skrbnika spletne strani.']]);
+                return response()->json(['error' => [
+                    'items' => 'Izdelek v košarici ima neveljaven ID, obrnite se na skrbnika spletne strani.'
+                    ]]);
 
             $items[$key] = $item;
+            $price += $item->price;
         }
+        session(['price' => $price]);
+        session(['items' => $items]);
 
-        $_SESSION['items'] = $items;
-
-        return;
+        return response()->json(['items' => $items, 'price' => $price]);
     }
 
     public function checkUserData()
@@ -73,8 +100,9 @@ class CheckoutController extends Controller
         }
 
         $data = request()->all();
-        $_SESSION['userData'] = $data;
 
-        return;
+        session(['userData' => $data]);
+
+        return response()->json(['data' => $data]);
     }
 }
