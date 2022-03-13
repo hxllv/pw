6e55306c8aafc7460bc32e0c63986e40621a6d42
NT <nace.tavcer20@gmail.com>
2022-03-13 12:28:47 +0100

    Added meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="keywords" content="pragwald, woodworks, unikatni, leseni, izdelki, les, deske, alkohol, ročno, izdelano, delavnica, javor, oreh, česnja"
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
