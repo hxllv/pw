bd8664c6acbd37c90719c10bb9cda90539e158e5
Nace Tavčer <46198891+hxllv@users.noreply.github.com>
2022-03-19 14:17:25 +0100

    To force css and js update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}?v=1.0.4" defer></script>
+    <script src="{{ asset('js/app.js') }}?v=1.0.5" defer></script>
     <script src="https://kit.fontawesome.com/be4cbb0046.js" crossorigin="anonymous"></script>
 
     @yield('scripts')
@@ -23,7 +23,7 @@
         rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}?v=1.0.3" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}?v=1.0.4" rel="stylesheet">
 
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
