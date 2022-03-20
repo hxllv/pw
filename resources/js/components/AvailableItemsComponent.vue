75b8c5e83ebf0af2c9f92fff5ca1b779eb3d5e9d
Nace Tavčer <46198891+hxllv@users.noreply.github.com>
2022-03-20 20:45:01 +0100

    fixed items not rendering on production

--- resources/js/components/AvailableItemsComponent.vue
+++ resources/js/components/AvailableItemsComponent.vue
@@ -66,14 +66,10 @@ export default {
         let tempTypes = JSON.parse(this.types);
         let tempItems = JSON.parse(this.items);
 
-        console.log(tempItems);
-
         tempTypes.forEach(type => {
             let tempArr = tempItems.filter(
                 item => item.type_id == type.id && item.available == 1
             );
-
-            console.log(tempArr);
 
             this.fullArr[`type_${type.id}`] = {
                 type: type,
@@ -82,9 +78,6 @@ export default {
         });
 
         this.notAvailArr = tempItems.filter(item => item.available == 0);
-
-        console.log(this.notAvailArr);
-        console.log(this.fullArr);
     },
     data() {
         return {
